70d9ad43f9662d3ff73d26f7826674fbceeb11cc
ZhenyuWang <13641039885@163.com>
2021-10-11 11:12:52 +0800

    refactor(app/utils/stackparser.js): 添加注释

--- backend/app/utils/stackparser.js
+++ backend/app/utils/stackparser.js
@@ -1,4 +1,5 @@
 'use strict';
+// 解析Error方法
 const ErrorStachParser = require('error-stack-parser');
 const { SourceMapConsumer } = require('source-map');
 const path = require('path');
@@ -11,9 +12,12 @@ module.exports = class StackParser {
   async parseStackTrack(stack, message) {
     const error = new Error(message);
     error.stack = stack;
+    // 获取error堆栈信息
     const stackFrame = ErrorStachParser.parse(error);
+    console.log('stackFrame', stackFrame);
     return await this.getOriginalErrorStack(stackFrame);
   }
+  // 获取原始错误堆栈信息
   async getOriginalErrorStack(stackFrame) {
     const origin = await this.getOriginPosition(stackFrame[0]);
     return origin;
@@ -30,8 +34,10 @@ module.exports = class StackParser {
       if (!fs.existsSync(sourceMapPath)) {
         return stackFrame;
       }
+      // 获取sourcemap内容
       const content = fs.readFileSync(sourceMapPath, 'utf-8');
       consumer = await new SourceMapConsumer(content, null);
+      // 将本次获取的sourcemap对象存放在缓存，方便下次使用
       this.consumers[fileName] = consumer;
     }
     const parseData = consumer.originalPositionFor({ line: lineNumber, column: columnNumber });
